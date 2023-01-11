b9979163c2bf509e7c418e840613b61640dd09c9
tiihmesquita <thiagomesquita17@gmail.com>
2023-01-11 01:09:44 -0300

    projeto banco digital

---
+++ projeto-banco-digital/src/ContaPoupanca.java
@@ -0,0 +1,11 @@
+public class ContaPoupanca extends Conta{
+
+    public void imprimirExtrato(){
+        System.out.println("*** Extrato Conta Poupanca ***");
+        super.imprimirInfosComuns();
+    }
+
+    
+
+    
+}
